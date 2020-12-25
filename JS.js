dc37449e985dbf72f8359571aa324c2aebfc0fd0
Evgenia <69460909+EvgeniyaYlanova@users.noreply.github.com>
2020-12-25 11:12:55 +0300

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,52 @@
+ //Слайдер 1
+
+ var swiper = new Swiper('.swiper1', {
+     slidesPerView: 1,
+     spaceBetween: 30,
+     loop: true,
+     pagination: {
+         el: '.swiper-pagination',
+         clickable: true,
+     },
+     navigation: {
+         nextEl: '.swiper-button-next',
+         prevEl: '.swiper-button-prev',
+     },
+ });
+ //Слайдер 1 //Слайдер 1
+
+ //Слайдер2
+
+ var swiper = new Swiper('.swiper2', {
+     direction: 'vertical',
+     slidesPerView: 1,
+     spaceBetween: 30,
+     mousewheel: true,
+     pagination: {
+         el: '.swiper-pagination',
+         clickable: true,
+     },
+ });
+
+ // Показывать окно в sec 10
+
+ const cards = document.querySelectorAll('.card');
+ const cardInfo = document.querySelectorAll('.card__info');
+
+ const toggleCard = (card) => {
+     card.classList.toggle('active');
+ }
+
+ cards.forEach((card, i) => {
+     card.addEventListener('click', () => {
+         toggleCard(cardInfo[i]);
+     });
+ });
+
+
+ // бургер меню
+
+ $('.burger-on').click(function () {
+     $(".menu").fadeToggle(100);
+     $('.menu').show(); // Показывает содержимое диалога.
+ });
